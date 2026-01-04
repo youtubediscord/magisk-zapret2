44fe4d970006178bbd67e0500d8b6cb6320a6f11
loop-uh <loop-uh@gmail.com>
2026-01-05 01:23:49 +0300

    v1.6.1: STUN improvements + UDP categories
    - Remove --filter-l7 from STUN (--payload in strategy is enough)
    - Move voice_stun to first position in categories
    - Add telegram_udp and whatsapp_udp categories
    - Expand UDP ports filter: 443,1400,50000-51000

--- zapret2/strategies-stun.sh
+++ zapret2/strategies-stun.sh
@@ -12,7 +12,7 @@ LISTS_DIR="$ZAPRET_DIR/lists"
 
 # List all available STUN strategies
 list_stun_strategies() {
-    echo "disabled fake_x6_stun_discord fakeknown_x6_stun_discord fake_x6_30_out_stun_discord fakeknown_x6_30_out_stun_discord fake_x6_2plus_stun_discord fakeknown_x6_2plus_stun_discord fakeknown_x3_stun_discord fake_x3_stun_discord fakeknown_x6_pad_stun_discord fake_x6_pad_stun_discord fakeknown_x6_10_out_stun_discord fake_x6_10_out_stun_discord fakeknown_x6_20_out_stun_discord fake_x6_20_out_stun_discord fakeknown_x3_2plus_stun_discord fake_x3_2plus_stun_discord fakeknown_x3_30_out_stun_discord fake_x3_30_out_stun_discord fakeknown_x3_pad_stun_discord fake_x3_pad_stun_discord fakeknown_x6_50_out_stun_discord fake_x6_50_out_stun_discord fakeknown_x3_10_out_stun_discord fake_x3_10_out_stun_discord fakeknown_x3_20_out_stun_discord fake_x3_20_out_stun_discord fakeknown_x10_stun_discord fake_x10_stun_discord fakeknown_x3_50_out_stun_discord fake_x3_50_out_stun_discord fakeknown_x6_100_out_stun_discord fake_x6_100_out_stun_discord fakeknown_x3_100_out_stun_discord fake_x3_100_out_stun_discord fake_x10_30_out_stun_discord fakeknown_x10_30_out_stun_discord fake_x10_2plus_stun_discord fakeknown_x10_2plus_stun_discord fake_x10_pad_stun_discord fakeknown_x10_pad_stun_discord fake_x10_10_out_stun_discord fakeknown_x10_10_out_stun_discord fake_x10_20_out_stun_discord fakeknown_x10_20_out_stun_discord fake_x10_50_out_stun_discord fakeknown_x10_50_out_stun_discord fake_x10_100_out_stun_discord fakeknown_x10_100_out_stun_discord"
+    echo "disabled fake_veryfast_stun_discord fake_x6_stun_discord fake_x6_stun_discord fake_x6_30_out_stun_discord fake_x6_30_out_stun_discord fake_x6_2plus_stun_discord fake_x6_2plus_stun_discord fake_x3_stun_discord fake_x3_stun_discord fake_x6_pad_stun_discord fake_x6_pad_stun_discord fake_x6_10_out_stun_discord fake_x6_10_out_stun_discord fake_x6_20_out_stun_discord fake_x6_20_out_stun_discord fake_x3_2plus_stun_discord fake_x3_2plus_stun_discord fake_x3_30_out_stun_discord fake_x3_30_out_stun_discord fake_x3_pad_stun_discord fake_x3_pad_stun_discord fake_x6_50_out_stun_discord fake_x6_50_out_stun_discord fake_x3_10_out_stun_discord fake_x3_10_out_stun_discord fake_x3_20_out_stun_discord fake_x3_20_out_stun_discord fake_x10_stun_discord fake_x10_stun_discord fake_x3_50_out_stun_discord fake_x3_50_out_stun_discord fake_x6_100_out_stun_discord fake_x6_100_out_stun_discord fake_x3_100_out_stun_discord fake_x3_100_out_stun_discord fake_x10_30_out_stun_discord fake_x10_30_out_stun_discord fake_x10_2plus_stun_discord fake_x10_2plus_stun_discord fake_x10_pad_stun_discord fake_x10_pad_stun_discord fake_x10_10_out_stun_discord fake_x10_10_out_stun_discord fake_x10_20_out_stun_discord fake_x10_20_out_stun_discord fake_x10_50_out_stun_discord fake_x10_50_out_stun_discord fake_x10_100_out_stun_discord fake_x10_100_out_stun_discord"
 }
 
 # Get STUN strategy options by name
@@ -36,132 +36,96 @@ get_stun_strategy_options() {
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=6"
             ;;
 
-        fakeknown_x6_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=6"
+        fake_x6_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=6"
             ;;
 
         fake_x6_30_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fake:blob=0x00:repeats=6"
             ;;
 
-        fakeknown_x6_30_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fakeknown:blob=stun:repeats=6"
-            ;;
-
         fake_x6_2plus_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fake:blob=0x00:repeats=6"
             ;;
 
-        fakeknown_x6_2plus_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fakeknown:blob=stun:repeats=6"
-            ;;
-
-        fakeknown_x3_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=3"
-            ;;
-
         fake_x3_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=3"
-            ;;
-
-        fakeknown_x6_pad_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=6+udplen:delta=2"
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=3"
             ;;
 
         fake_x6_pad_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=6+udplen:delta=2"
-            ;;
-
-        fakeknown_x6_10_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=6"
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=6+udplen:delta=2"
             ;;
 
         fake_x6_10_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=6"
-            ;;
-
-        fakeknown_x6_20_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fakeknown:blob=stun:repeats=6"
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=6"
             ;;
 
         fake_x6_20_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fake:blob=0x00:repeats=6"
-            ;;
-
-        fakeknown_x3_2plus_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fakeknown:blob=stun:repeats=3"
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fake:blob=stun:repeats=6"
             ;;
 
         fake_x3_2plus_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fake:blob=0x00:repeats=3"
-            ;;
-
-        fakeknown_x3_30_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fakeknown:blob=stun:repeats=3"
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fake:blob=stun:repeats=3"
             ;;
 
         fake_x3_30_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fake:blob=0x00:repeats=3"
             ;;
 
-        fakeknown_x3_pad_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=3+udplen:delta=2"
-            ;;
-
         fake_x3_pad_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=3+udplen:delta=2"
             ;;
 
-        fakeknown_x6_50_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fakeknown:blob=stun:repeats=6"
+        fake_x6_50_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fake:blob=stun:repeats=6"
             ;;
 
         fake_x6_50_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fake:blob=0x00:repeats=6"
             ;;
 
-        fakeknown_x3_10_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=3"
+        fake_x3_10_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=3"
             ;;
 
         fake_x3_10_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=3"
             ;;
 
-        fakeknown_x3_20_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fakeknown:blob=stun:repeats=3"
+        fake_x3_20_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fake:blob=stun:repeats=3"
             ;;
 
         fake_x3_20_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fake:blob=0x00:repeats=3"
             ;;
 
-        fakeknown_x10_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         fake_x10_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x3_50_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fakeknown:blob=stun:repeats=3"
+        fake_x3_50_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fake:blob=stun:repeats=3"
             ;;
 
         fake_x3_50_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fake:blob=0x00:repeats=3"
             ;;
 
-        fakeknown_x6_100_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fakeknown:blob=stun:repeats=6"
+        fake_x6_100_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fake:blob=stun:repeats=6"
             ;;
 
         fake_x6_100_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fake:blob=0x00:repeats=6"
             ;;
 
-        fakeknown_x3_100_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fakeknown:blob=stun:repeats=3"
+        fake_x3_100_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fake:blob=stun:repeats=3"
             ;;
 
         fake_x3_100_out_stun_discord)
@@ -172,56 +136,56 @@ get_stun_strategy_options() {
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x10_30_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_30_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d30 --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         fake_x10_2plus_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x10_2plus_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_2plus_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=2+ --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         fake_x10_pad_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=10+udplen:delta=2"
             ;;
 
-        fakeknown_x10_pad_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=10+udplen:delta=2"
+        fake_x10_pad_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=10+udplen:delta=2"
             ;;
 
         fake_x10_10_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x10_10_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_10_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d10 --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         fake_x10_20_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x10_20_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_20_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d20 --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         fake_x10_50_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x10_50_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_50_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d50 --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         fake_x10_100_out_stun_discord)
             echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fake:blob=0x00:repeats=10"
             ;;
 
-        fakeknown_x10_100_out_stun_discord)
-            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fakeknown:blob=stun:repeats=10"
+        fake_x10_100_out_stun_discord)
+            echo "$filter --payload=stun,discord_ip_discovery --out-range=-d100 --lua-desync=fake:blob=stun:repeats=10"
             ;;
 
         # ==================== END STUN STRATEGIES ====================
