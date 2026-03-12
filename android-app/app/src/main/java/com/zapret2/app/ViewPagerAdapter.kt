094a175a08ac37807217f0f89c9a8a36332d6e20
loop-uh <loop-uh@gmail.com>
2026-03-12 17:36:59 +0300

    добавлен редактор hosts

--- android-app/app/src/main/java/com/zapret2/app/ViewPagerAdapter.kt
+++ android-app/app/src/main/java/com/zapret2/app/ViewPagerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
-    override fun getItemCount(): Int = 7
+    override fun getItemCount(): Int = 8
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
@@ -15,8 +15,9 @@ class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapte
             2 -> PresetsFragment()
             3 -> ConfigEditorFragment()
             4 -> HostlistsFragment()
-            5 -> LogsFragment()
-            6 -> AboutFragment()
+            5 -> HostsEditorFragment()
+            6 -> LogsFragment()
+            7 -> AboutFragment()
             else -> ControlFragment()
         }
     }
