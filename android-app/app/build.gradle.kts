6f9076204c1abb03cf0741283ce4a8ba27d64d84
loop-uh <loop-uh@gmail.com>
2026-07-24 23:26:57 +0300

    Release 2.1.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.zapret2.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2010000
-        versionName = "2.1.0"
+        versionCode = 2010002
+        versionName = "2.1.2"
     }
 
     signingConfigs {
